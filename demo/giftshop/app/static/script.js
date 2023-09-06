a7c050cd8e2cc9ecf37d13b5f094acc3359f9768
AMBIRASHAH-PRAJAPATI <amrajprajapato@gmail.com>
2023-09-06 23:51:45 +0530

    adding gift shope

--- demo/giftshop/app/static/script.js
+++ demo/giftshop/app/static/script.js
@@ -50,3 +50,102 @@ function setTheme(theme) {
 
     localStorage.setItem('theme', theme);
 }
+
+// scrolling banner
+const a = [
+    "/openagent/assets/banner2.png",
+    "/openagent/assets/banner3.png",
+    "/openagent/assets/banner4.png",
+    "/openagent/assets/banner5.png",
+    "/openagent/assets/banner6.png",
+    "/openagent/assets/banner7.png"
+  ];
+  const next = document.querySelector(".r");
+  const prev = document.querySelector(".l");
+  const img = document.querySelector(".bannerimg");
+  const circle = document.querySelectorAll(".bannercircle");
+  var i = 0;
+  let colorcircleimg = i =>{
+    img.src = a[i];
+    circle.forEach((e) => {
+      e.style.background = "rgb(216,209,209)";
+    });
+    circle[i].style.background = "rgba(80, 80, 80, 0.819)";
+ }
+  img.addEventListener("load",()=>{
+    setTimeout(()=>{
+    i= i>=a.length-1 ? 0 : i+1;
+    colorcircleimg(i);
+  },5000)
+  })
+  next.addEventListener("click", function () {
+   i= i >=a.length-1 ? 0 : i+1;
+   colorcircleimg(i);
+  });
+  prev.addEventListener("click", function () {
+    i = i <=0 ? 5 : i-1;
+    colorcircleimg(i);
+  });
+
+
+// Function to create item boxes
+function createItemBoxes(itemData) {
+    const itemContainer = document.getElementById('productcontainer');
+    itemContainer.innerHTML = '';
+  
+    itemData.forEach(item => {
+      const productDiv = document.createElement('div');
+      productDiv.classList.add('product','col','d-flex', 'flex-column');
+  
+      // Create the product image element
+      const img = document.createElement('img');
+      img.classList.add('product-img');
+      img.src = item.thumbnail;
+      img.alt = item.title;
+  
+      // Create the product info div
+      const productInfoDiv = document.createElement('div');
+      productInfoDiv.classList.add('product-info', 'd-flex', 'flex-column');
+  
+      // Create the title element
+      const title = document.createElement('h4');
+      title.classList.add('mb-2');
+      title.textContent = item.title;
+  
+      // Create the price element
+      const price = document.createElement('h6');
+      price.classList.add('mb-2', 'text-muted');
+      price.innerHTML = `<i class="fa fa-tag"></i> Price: ${item.price}`;
+  
+      // Create the description element
+      const description = document.createElement('p');
+      description.classList.add('mt-3', 'mb-3');
+      description.innerHTML = `<i class="fa fa-info-circle"></i> Description: ${item.snippet}`;
+  
+      // Create the "Buy Now" button
+      const buyNowButton = document.createElement('a');
+      buyNowButton.href = ''; // Add your URL here
+      buyNowButton.classList.add('btn', 'btn-primary', 'mt-auto');
+      buyNowButton.innerHTML = `<i class="fa fa-shopping-cart"></i> Buy Now`;
+  
+      // Append all elements to the main product div
+      productInfoDiv.appendChild(title);
+      productInfoDiv.appendChild(price);
+      productDiv.appendChild(img);
+      productDiv.appendChild(productInfoDiv);
+      productDiv.appendChild(description);
+      productDiv.appendChild(buyNowButton);
+  
+      // Append the item box to the container
+      itemContainer.appendChild(productDiv);
+    });
+  }
+  
+  // Fetch data from a JSON file
+  fetch('giftitems.json')
+    .then(response => response.json())
+    .then(data => {
+      createItemBoxes(data);
+    })
+    .catch(error => console.error('Error fetching data:', error));
+  
